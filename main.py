f5395c5655016482fd9fc4d9728992ceb7e09643
masih4 <amirreza.mahbod@gmail.com>
2024-07-03 17:43:08 +0200

    make clean code for keras models and run them

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.applications.vgg19 import VGG19, preprocess_input
+#from tensorflow.keras.applications.vgg19 import VGG19, preprocess_input
+#from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input
+#from tensorflow.keras.applications.resnet import ResNet50, preprocess_input
+from tensorflow.keras.applications.efficientnet_v2 import EfficientNetV2M, preprocess_input
+
+
+
+
 from tensorflow.keras.preprocessing.image import img_to_array, array_to_img
 from tensorflow.keras.models import Model
 from sklearn.metrics.pairwise import cosine_similarity
@@ -10,10 +17,10 @@ import cv2
 from metric import *
 from tqdm import tqdm
 
-size = 128
-
+size = 32
+top_n = 5
 # Load the dataset
-data = np.load('../dataset/128/breastmnist_128.npz')
+data = np.load('../dataset/28/breastmnist.npz')
 
 train_images = data['train_images']
 train_labels = data['train_labels']
@@ -39,36 +46,12 @@ if len(train_images_resized.shape) == 3:
 
 print('number of classes:', len(np.unique(train_labels)))
 
-model = VGG19(weights='imagenet', include_top=False, input_shape=(size, size, 3), pooling='avg')
+model = EfficientNetV2M(weights='imagenet', include_top=False, input_shape=(size, size, 3), pooling='avg')
 
 
 # Extract features
 train_features = model.predict(train_images_rgb, batch_size=8)
 test_features = model.predict(test_images_rgb, batch_size=8)
-
-# Build the retrieval model
-def retrieve_similar_images(query_features, databank_features, top_n=5):
-    similarities = cosine_similarity(query_features, databank_features)
-    indices = np.argsort(similarities, axis=1)[:, -top_n:][:, ::-1]
-    return indices
-
-# Retrieve the top 5 similar images for each test image
-top_n = 5
-retrieved_indices = retrieve_similar_images(test_features, train_features, top_n)
-
-
-
-#evaluate emad code
-list_res = []
-for i in range(len(test_labels)):
-    res = map_k(test_labels[i], train_labels[retrieved_indices[i]], k=5)
-    list_res.append(res)
-
-np.mean(list_res)
-
-
-
-
 
 
 ap_k_list, hit_rate_k_list, mmv_k_list = [], [], []
@@ -97,33 +80,10 @@ for i in tqdm(range(len(test_features))):
 mean_ap_k_list = np.mean(ap_k_list)
 mean_hit_rate_k_list = np.mean(hit_rate_k_list)
 mean_mmv_k_list = np.mean(mmv_k_list)
-print(
-    f"pretrained_mean_ap_k_list: {mean_ap_k_list} pretrained_mean_hit_rate_k_list: {mean_hit_rate_k_list}"
-    f" pretrained_mean_mmv_k_list: {mean_mmv_k_list}")
+print(f"mean_ap_k_list: {mean_ap_k_list} \n"
+      f"mean_hit_rate_k_list: {mean_hit_rate_k_list} \n"
+      f" mean_mmv_k_list: {mean_mmv_k_list} \n")
 
 
 
 
-
-
-
-
-
-
-# # Evaluate (ineternet)
-# def evaluate_retrieval(retrieved_indices, test_labels, train_labels):
-#     precisions = []
-#     aps = []
-#     for i, indices in enumerate(retrieved_indices):
-#         retrieved_labels = train_labels[indices]
-#         true_label = test_labels[i]
-#         precision = precision_score([true_label] * top_n, retrieved_labels, average='macro')
-#         ap = average_precision_score([true_label] * top_n, retrieved_labels)
-#         precisions.append(precision)
-#         aps.append(ap)
-#     return np.mean(precisions), np.mean(aps)
-#
-# mean_precision, mean_ap = evaluate_retrieval(retrieved_indices, test_labels, train_labels)
-#
-# print(f'Mean Precision: {mean_precision}')
-# print(f'Mean Average Precision: {mean_ap}')
