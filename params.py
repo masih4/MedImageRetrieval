f92eeac31183faf1e6afa47e6a42d30c077daa73
masih4 <amirreza.mahbod@gmail.com>
2024-07-15 17:12:45 +0200

    add biomedclip for 2D

--- params.py
+++ params.py
@@ -2,12 +2,13 @@
 opts = {}
 opts['resize'] = 32
 opts['top_k'] = 5
-opts['data_path'] = '../dataset/28/synapsemnist3d.npz'
-opts['pretrained_network_name'] = 'EfficientNetV2M'
+opts['data_path'] = '../dataset/28/pathmnist.npz'
+opts['pretrained_network_name'] = 'biomedclip'
 # 'VGG19'
 # 'ResNet50'
 # 'DenseNet121'
 # 'EfficientNetV2M'
+# 'biomedclip'
 
 opts['save_train_hard'] = '../dataset/28/synapsemnist3d_28/train/'
 opts['save_test_hard'] = '../dataset/28/synapsemnist3d_28/test/'
