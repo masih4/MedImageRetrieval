d49f9a7065c78b1560e12edf5b9b0f52b8db2017
masih4 <amirreza.mahbod@gmail.com>
2024-09-03 15:01:32 +0200

    update sup materials

--- params.py
+++ params.py
@@ -1,9 +1,9 @@
 # set all hyper parameters
 opts = {}
-opts['resize'] = 128
+opts['resize'] = 64
 opts['top_k'] = 5
-opts['data_path'] = '../dataset/128/pathmnist_128.npz'
-opts['pretrained_network_name'] = 'virchow2'
+opts['data_path'] = '../dataset/64/adrenalmnist3d_64.npz'
+opts['pretrained_network_name'] = 'UNI'
 # 'VGG19'
 # 'ResNet50'
 # 'DenseNet121'
@@ -18,8 +18,8 @@ opts['pretrained_network_name'] = 'virchow2'
 opts['tsne'] = False
 opts['tsne_title'] = 'BreastMNIST'
 
-opts['save_train_hard'] = '../dataset/64/synapsemnist3d_64/train/'
-opts['save_test_hard'] = '../dataset/64/synapsemnist3d_64/test/'
+opts['save_train_hard'] = '../dataset/64/adrenalmnist3d_64/train/'
+opts['save_test_hard'] = '../dataset/64/adrenalmnist3d_64/test/'
 opts['save_figures'] = '../results/figures/'
 opts['save_figures_tsen'] = '../results/figures/tsne/'
 
